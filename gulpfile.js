a63752dfc86a0bc30c88ded5485caf1a9e871ce7
eid sid <eidsiddiedis@gmail.com>
2021-10-30 00:21:13 +0200

    set up environment

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+// list dependences
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass'),
+    prefix = require('gulp-autoprefixer'),
+    minify = require('gulp-clean-css'),
+    terser = require('gulp-terser'),
+    imgmin = require('gulp-imagemin'),
+    imgwebp = require('gulp-webp');
+
+// create functions
+
+// scss
+compilescss => {
+    return src('./project/css/*.scss')
+        .pipe(sass())
+        .pipe(prefix('last 2 versions'))
+        .pipe(minify())
+        .pipe(dest('./dist/css'))
+}
+
+//js
+jsmin => {
+    return src('./project/js/main.js')
+        .pipe(terser())
+        .pipe(dest('./dist/js'))
+}
+
+//images
+imgsoptimize => {
+    return src('./project/images/*.{jpg,png}')
+        .pipe(imgmin(
+            [imgmin.mozjpeg({ quality: 80, progressive: true }),
+                imgmin.optipng({ optimizationLevel: 2 }),
+            ]
+        )).pipe(dest('./dist/imgs'))
+}
+
+//webp images
+webpImages => {
+    return src('./dist/imgs/*.{jpg,png}')
+        .pipe(webpImages())
+        .pipe(dest('./dist/imgs'))
+}
+
+watchtask => {
+        watch('./project/css/*.scss', compilescss);
+        watch('./project/js/*.js', jsmin);
+        watch('./project/images/*.{jpg,png}', imgsoptimize);
+        watch('./dist/imgs/*.{jpg,png}', webpImages);
+    }
+    //defult gulp
+
+exports.default = series(
+    compilescss,
+    jsmin,
+    imgsoptimize,
+    webpImages,
+    watchtask
+)
---
+++ project/js/main.js
@@ -0,0 +1,4 @@
+let a = "welcome eid sayed taha",
+    b = 1233,
+    c = true,
+    j = 102;
