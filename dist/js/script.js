81c0f08b63d4ad097b836e792aceb3319e27bc97
eid sid <eidsiddiedis@gmail.com>
2021-11-06 20:16:50 +0200

    resize images

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,83 +1,21 @@
-const header = document.querySelector('.header'),
-    menu_btn_open = header.querySelector('.menubtn .open'),
-    menu_btn_close = header.querySelector('.menubtn .close');
-
-
-[menu_btn_open, menu_btn_close].forEach(el => {
-    el.addEventListener('click', () => {
-        menu_stats();
-    });
-})
-
-header.querySelectorAll('nav ul li').forEach((el) => {
-    el.addEventListener('click', () => {
-        menu_stats();
-    })
-})
-
+const header = document.querySelector(".header"),
+    menu_btn_open = header.querySelector(".menubtn .open"),
+    menu_btn_close = header.querySelector(".menubtn .close");
+[menu_btn_open, menu_btn_close].forEach((e => { e.addEventListener("click", (() => { menu_stats() })) })), header.querySelectorAll("nav ul li").forEach((e => { e.addEventListener("click", (() => { menu_stats() })) }));
 let menu_stats = () => {
-    [menu_btn_open, menu_btn_close, header].forEach(el => {
-        el.classList.toggle('active');
-    })
-
-}
-
-const guestscount_container = document.querySelector('.banner .banner-content .guests .guests_count ');
-
-let guests = 1;
-let guests_count_increase = guestscount_container.querySelector('.updown .up');
-let guests_count_decrease = guestscount_container.querySelector('.updown .down');
-
-guests_count_increase.addEventListener('click', () => {
-
-    if (guests >= 10) {
-        guests = 10;
-    } else { guests += 1; }
-    guests_count_fun(guests);
-
-})
-guests_count_decrease.addEventListener('click', () => {
-
-    if (guests <= 0) {
-        guests = 0;
-
-    } else { guests -= 1; }
-    guests_count_fun(guests);
-
-})
-
-let guests_count_fun = (guests) => {
-    guestscount_container.querySelector('input').value = guests;
-}
-
-
-let offercontainer = document.querySelector('.offer-container'),
-    card = document.querySelectorAll('.offer-container .card');
-
-
-card.forEach(el => {
-    const card_open = el.querySelector('.open-layer'),
-        card_footer = el.querySelector('.card-footer'),
-        card_close = card_footer.querySelector(' .close');
-
-    card_open.addEventListener('click', () => {
-        card_footer.classList.add('active');
-    })
-
-    card_close.addEventListener('click', () => {
-        card_footer.classList.remove('active');
-    })
-})
-
-window.onscroll = () => {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        header.classList.add('sticky');
-    } else {
-        header.classList.remove('sticky');
-
-    }
-}
-document.querySelector('.top').addEventListener('click', () => {
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-
-})
+    [menu_btn_open, menu_btn_close, header].forEach((e => { e.classList.toggle("active") }))
+};
+const guestscount_container = document.querySelector(".banner .banner-content .guests .guests_count ");
+let guests = 1,
+    guests_count_increase = guestscount_container.querySelector(".updown .up"),
+    guests_count_decrease = guestscount_container.querySelector(".updown .down");
+guests_count_increase.addEventListener("click", (() => { guests >= 10 ? guests = 10 : guests += 1, guests_count_fun(guests) })), guests_count_decrease.addEventListener("click", (() => { guests <= 0 ? guests = 0 : guests -= 1, guests_count_fun(guests) }));
+let guests_count_fun = e => { guestscount_container.querySelector("input").value = e },
+    offercontainer = document.querySelector(".offer-container"),
+    card = document.querySelectorAll(".offer-container .card");
+card.forEach((e => {
+    const t = e.querySelector(".open-layer"),
+        n = e.querySelector(".card-footer"),
+        c = n.querySelector(" .close");
+    t.addEventListener("click", (() => { n.classList.add("active") })), c.addEventListener("click", (() => { n.classList.remove("active") }))
+})), window.onscroll = () => { document.body.scrollTop > 100 || document.documentElement.scrollTop > 100 ? header.classList.add("sticky") : header.classList.remove("sticky") }, document.querySelector(".top").addEventListener("click", (() => { document.documentElement.scrollTop = 0 }));
